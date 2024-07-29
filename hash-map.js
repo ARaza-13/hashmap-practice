e75073a31c790b3e0a9533d374162b43f5bd31ac
Ali Raza <99924946+ARaza-13@users.noreply.github.com>
2024-07-29 00:45:17 -0400

    Added a _hash(key) method to compute a hash code for a given key and map it to the index within the bucket array

--- hash-map.js
+++ hash-map.js
@@ -1,4 +1,4 @@
-class HashMap {
+export default class HashMap {
   constructor(initialSize = 16, loadFactor = 0.75) {
     this.size = initialSize;
     this.buckets = Array(this.size)
@@ -6,5 +6,17 @@ class HashMap {
       .map(() => []);
     this.loadFactor = loadFactor;
     this.count = 0; // track number of key-value pairs
+  }
+
+  _hash(key) {
+    let hashCode = 0;
+
+    const primeNumber = 31;
+    for (let i = 0; i < key.length; i++) {
+      hashCode = primeNumber * hashCode + key.charCodeAt(i);
+      hashCode = hashCode % this.size;
+    }
+
+    return hashCode;
   }
 }
---
+++ index.js
@@ -0,0 +1,6 @@
+import HashMap from "./hash-map.js";
+
+const hash = new HashMap();
+
+console.log(hash._hash("Tifa"));
+console.log(hash._hash("Cloud"));
