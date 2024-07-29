eefca3969a60bdc73c913ed176bdc8d2d13272cb
Ali Raza <99924946+ARaza-13@users.noreply.github.com>
2024-07-29 16:40:40 -0400

    Added hash(key) method to compute the hash code for a given key

--- hash-set.js
+++ hash-set.js
@@ -7,4 +7,16 @@ export default class HashSet {
     this.loadFactor = loadFactor;
     this.count = 0; // track number of keys
   }
+
+  _hash(key) {
+    let hashCode = 0;
+
+    const primeNumber = 31;
+    for (let i = 0; i < key.length; i++) {
+      hashCode = primeNumber * hashCode + key.charCodeAt(i);
+      hashCode = hashCode % this.size;
+    }
+
+    return hashCode;
+  }
 }
