b25b671e3bbe4b700ca211a0e25d8aa5e76673d6
Ali Raza <99924946+ARaza-13@users.noreply.github.com>
2024-07-29 16:05:57 -0400

    Added more test cases

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ import HashMap from "./hash-map.js";
 
 const hash = new HashMap();
 
-hash.set("Cloud", "Strife");
+hash.set("Tifa", "LOCKHART");
+console.log(hash.get("Tifa")); // LOCKHART
+
 hash.set("Tifa", "Lockhart");
+console.log(hash.get("Tifa")); // Lockhart
 
 hash.clear();
 console.log(hash.entries()); // []
@@ -26,7 +29,7 @@ console.log(hash.get("Aerith")); // Gainsborough
 console.log(hash.has("Aerith")); // true
 console.log(hash.keys()); // ['Tifa', 'Cloud', 'Aerith']
 console.log(hash.values()); // ['Lockhart', 'Strife', 'Gainsborough']
-console.log(hash.entries()); // [ ['Tifa', 'Lockhart'], ['Cloud', 'Strife'], ['Aerith', 'Gainsborough'] ]
+console.log(hash.entries()); // [['Tifa', 'Lockhart'], ['Cloud', 'Strife'], ['Aerith', 'Gainsborough']]
 
 console.log(hash.remove("Aerith")); // true
 console.log(hash.get("Aerith")); // null
@@ -35,4 +38,4 @@ console.log(hash.has("Aerith")); // false
 console.log(hash.length()); // 2
 console.log(hash.keys()); // ['Tifa', 'Cloud']
 console.log(hash.values()); // ['Lockhart', 'Strife']
-console.log(hash.entries()); // [ ['Tifa', 'Lockhart'], ['Cloud', 'Strife'] ]
+console.log(hash.entries()); // [['Tifa', 'Lockhart'], ['Cloud', 'Strife']]
